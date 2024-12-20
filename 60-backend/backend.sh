8c4d6d2c87f3dd257995e8c8452dc1d0651fcb86
ani <anitha.s.goutham@gmail.com>
2024-12-21 03:51:00 +0800

    terraform

--- 60-backend/backend.sh
+++ 60-backend/backend.sh
@@ -4,3 +4,4 @@ component=$1
 environment=$2
 echo "Component: $component, Environment: $environment"
 dnf install ansible -y
+ansible-pull -i localhost, -U https://github.com/anithagoutham/expense-ansible-roles-tf.git main.yaml -e component=$component -e environment=$environment
--- 60-backend/main.tf
+++ 60-backend/main.tf
@@ -47,3 +47,14 @@ resource "null_resource" "backend" {
   }
 }
 
+resource "aws_ec2_instance_state" "backend" {
+  instance_id = module.backend.id
+  state = "stopped"
+  depends_on = [null_resource.backend]
+}
+
+resource "aws_ami_from_instance" "backend" {
+  name = local.resource_name
+  source_instance_id = module.backend.id
+  depends_on = [aws_ec2_instance_state.backend]
+}
